e192ba45ee02726d8e38974173b37d27e886d0e1
SerginhoLeal <sergiojunioleal@hotmail.com>
2023-07-18 16:56:17 -0300

    [TESTING] deploy vercel

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import * as Styles from './styles';
+
+export default function Home(){
+  return(
+    <Styles.Container>
+      <h1>Marks Joias</h1>
+    </Styles.Container>
+  )
+}
---
+++ src/pages/home/styles.ts
@@ -0,0 +1,13 @@
+import styles from 'styled-components';
+
+export const Container = styles.main`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  height: 100vh;
+
+  h1{
+    font-family: 'Poppins', sans-serif;
+  }
+`;

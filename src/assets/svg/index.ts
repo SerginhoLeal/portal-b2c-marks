662a93e40672e4daf7768b9c7827696eeb733517
SerginhoLeal <sergiojunioleal@hotmail.com>
2023-07-25 13:05:33 -0300

    [NEW] svgs

---
+++ src/assets/svg/index.ts
@@ -0,0 +1,33 @@
+import { ReactComponent as MasterCard } from './MasterCard.svg';
+import { ReactComponent as Diners } from './Diners.svg';
+import { ReactComponent as Elo } from './Elo.svg';
+import { ReactComponent as Hiper } from './Hiper.svg';
+import { ReactComponent as HiperCard } from './HiperCard.svg';
+import { ReactComponent as Amex } from './Amex.svg';
+import { ReactComponent as Pix } from './Pix.svg';
+import { ReactComponent as Boleto } from './Boleto.svg';
+import { ReactComponent as Delivery } from './Delivery.svg';
+import { ReactComponent as Logo } from './Logo.svg';
+import { ReactComponent as NameLogo } from './NameLogo.svg';
+import { ReactComponent as Qrcode } from './Qrcode.svg';
+import { ReactComponent as ArrowDown } from './ArrowDown.svg';
+import { ReactComponent as SearchNormal } from './SearchNormal.svg';
+import { ReactComponent as Google } from './Google.svg';
+
+export {
+  MasterCard,
+  Diners,
+  Elo,
+  Hiper,
+  HiperCard,
+  Amex,
+  Pix,
+  Boleto,
+  Delivery,
+  Logo,
+  NameLogo,
+  Qrcode,
+  ArrowDown,
+  SearchNormal,
+  Google
+};

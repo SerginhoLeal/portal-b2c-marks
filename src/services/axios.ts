da055f8c8b889a51727eb28772aa15ffaadbc2be
SerginhoLeal <sergiojunioleal@hotmail.com>
2023-08-01 18:45:40 -0300

    [FIX] adding endpoint

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export const api = axios.create({
   // baseURL: (process.env.BASE_URL || 'http://192.168.15.148:3333'),
-  baseURL: '/api',
+  baseURL: 'https://portal-b2c-marks.vercel.app/api',
   // baseURL: 'http://192.168.15.91:3333',
 })

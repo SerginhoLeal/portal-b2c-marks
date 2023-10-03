00e81b8019a83c8b1daed68a38322e0a496ab4df
SerginhoLeal <sergiojunioleal@hotmail.com>
2023-10-03 15:12:30 -0300

    [refactor] remove texts

--- src/common/footer/index.tsx
+++ src/common/footer/index.tsx
@@ -17,27 +17,26 @@ export const Footer: React.FC = () => {
           <Styles.OptionsForClient>
             <Text color='white' fontWeight='medium' fontSize='small'>INFORMAÇÕES DA EMPRESA</Text>
             <br />
-            <Text color='white' fontWeight='normal' fontSize='small'>Sobre a <strong>Marks Joias</strong></Text>
-            <Text color='white' fontWeight='normal' fontSize='small'>Politica para Revendedores</Text>
+            <Text color='white' fontWeight='light' fontSize='xsmall'>Sobre a <strong>Marks Joias</strong></Text>
           </Styles.OptionsForClient>
 
-          <Styles.OptionsForClient>
+          {/* <Styles.OptionsForClient>
             <Text color='white' fontWeight='medium' fontSize='small'>SERVIÇOS</Text>
             <br />
             <Text color='white' fontWeight='normal' fontSize='small'>Seu ticket</Text>
             <Text color='white' fontWeight='normal' fontSize='small'>Sacoleiras</Text>
-          </Styles.OptionsForClient>
+          </Styles.OptionsForClient> */}
           
           <Styles.OptionsForClient>
             <Text color='white' fontWeight='semi_bold' fontSize='small'>ATENDIMENTO AO CLIENTE</Text>
             <br />
-            <Text color='white' fontWeight='normal' fontSize='small'>Contate-nos</Text>
-            <Text color='white' fontWeight='normal' fontSize='small'>Trocas e Devoluções</Text>
-            <Text color='white' fontWeight='normal' fontSize='small'>Pontos Bônus</Text>
+            <Text color='white' fontWeight='light' fontSize='xsmall'>Contate-nos</Text>
+            <Text color='white' fontWeight='light' fontSize='xsmall'>Trocas e Devoluções</Text>
+            <Text color='white' fontWeight='light' fontSize='xsmall'>Pontos Bônus</Text>
           </Styles.OptionsForClient>
         </Styles.Info>
 
-        <Text color='white' fontWeight='normal' fontSize='xsmall'>©2023-2023 Todos os direitos reservados <strong>MARK’S JOIAS</strong></Text>
+        {/* <Text color='white' fontWeight='normal' fontSize='xsmall'>©2023-2023 Todos os direitos reservados <strong>MARK’S JOIAS</strong></Text> */}
 
       </Styles.Center>
       <Styles.Right>
--- src/common/footer/styles.ts
+++ src/common/footer/styles.ts
@@ -1,15 +1,22 @@
 import styled from 'styled-components';
 
+import Link from 'next/link';
+
 export const Container = styled.footer`
   display: flex;
 
   align-items: center;
 
   width: 100%;
-  height: 300px;
+  height: 200px;
 
   background: rgb(250,11,91);
   background: linear-gradient(90deg, rgba(250,11,91,1) 0%, rgba(57,95,245,1) 100%, rgba(0,212,255,1) 100%);
+
+  @media (max-width: 600px){
+    flex-direction: column;
+    height: auto;
+  }
 `;
 
 export const Left = styled.div`
@@ -20,6 +27,10 @@ export const Left = styled.div`
 
   width: 30%;
   height: 100%;
+
+  @media (max-width: 600px){
+    display: none;
+  }
 `;
 
 export const Center = styled.div`
@@ -33,12 +44,24 @@ export const Center = styled.div`
   justify-content: center;
 
   padding: 20px 10px;
+
+  @media (max-width: 900px){
+    height: auto;
+  }
+
 `;
 
 export const Info = styled.div`
   display: flex;
 
-  height: 100%;
+  flex-wrap: wrap;
+
+  @media (max-width: 900px){
+    height: auto;
+    div {
+      margin: 0 20px 7px 0;
+    }
+  }
 `;
 
 export const OptionsForClient = styled.div`
@@ -50,6 +73,12 @@ export const OptionsForClient = styled.div`
 
   height: 100%;
 
+  @media (max-width: 900px){
+    height: auto;
+    br {
+      display: none;
+    }
+  }
 `;
 
 // Right
@@ -62,12 +91,20 @@ export const Right = styled.div`
 
   padding: 20px 10px;
 
-  width: 50%;
+  width: 500px;
   height: 100%;
+
+  @media (max-width: 600px){
+    width: 100%;
+    padding: 10px;
+  }
 `;
 
 export const Cards = styled.div`
   svg:first-child(){
     margin: 100px;
   };
+`;
+
+export const LinkStyles = styled(Link)`
 `;

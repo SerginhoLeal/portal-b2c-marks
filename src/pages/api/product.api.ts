27f193a2e940644ec1f0313f816432b110bfe021
SerginhoLeal <sergiojunioleal@hotmail.com>
2023-10-03 15:07:59 -0300

    [new] create api to the all products

---
+++ src/pages/api/product.api.ts
@@ -0,0 +1,40 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+import Stripe from "stripe";
+
+import { stripe, prisma } from "@services";
+
+export default async(request: NextApiRequest, response: NextApiResponse) => {
+  const {product_id} = request.body;
+  console.log("product_id: ", product_id);
+  
+  if(request.method === 'POST'){
+    const res = await prisma.product.findMany({
+      where: {
+        id: product_id
+      },
+      include: {
+        files: {
+          select: {
+            name: true,
+            file: true
+          }
+        },
+        category: {
+          select: {
+            name: true
+          }
+        },
+        sellers: {
+          select: {
+            id: true
+          }
+        }
+      }
+    });
+
+    return response.status(201).json(res);
+  };
+
+  return response.status(405).json({ error: 'Method Not Allowed' })
+};

a68b39bbb02da2018f4afc4d4c1de53d9cffb9b4
SerginhoLeal <sergiojunioleal@hotmail.com>
2023-10-03 13:58:53 -0300

    [refactor] new product card

--- src/pages/products/product/index.tsx
+++ src/pages/products/product/index.tsx
@@ -1,9 +1,7 @@
-import React from 'react';
-
 import Image from 'next/image';
 
 import { ProductObject } from '@/interfaces';
-import { Text } from '..';
+import { Text } from '@common';
 
 import * as Styles from './styles';
 
@@ -11,21 +9,32 @@ interface Props {
   data: Array<ProductObject>;
 };
 
-export const Products: React.FC<Props> = ({ data }: Props) => {
-  if(data.length === 0){
+const Product: React.FC<Props> = ({ data }: Props) => {
+  if(data === undefined || data.length === 0){
     return <Styles.SkeletonStyle quantity={6} width={300} height={300} />
   };
 
   return data.map((product) => (
     <Styles.Container key={product.id} href={`/product/${product.id}`}>
 
-      <Image src={product.image} width="300" height="300" alt={product.name} style={{ borderRadius: 5 }} />
+      <Image
+        src={product.files[0]?.file}
+        width="300"
+        height="300"
+        alt={product.name}
+        style={{
+          borderRadius: 5,
+          objectFit: 'cover',
+        }}
+      />
 
       <Styles.Info>
-        <Text fontStyle='italic' fontSize='xsmall' color='white'>{product.name}</Text>
+        <Text fontStyle='italic' fontSize='xsmall' color='white'>{product.files[0].name}</Text>
         <Text fontStyle='italic' fontWeight='semi_bold' color='white'>{product.price}</Text>
       </Styles.Info>
 
     </Styles.Container>
   ))
 };
+
+export default Product;
